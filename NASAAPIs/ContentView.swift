5307532e0e8a6a65b72e2b1f2d9667d56111fa22
Ivan Quintana <ivanquintana@Ivans-MacBook-Pro.local>
2023-07-07 22:19:54 -0600

    initial config

---
+++ NASAAPIs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NASAAPIs
+//
+//  Created by Ivan Quintana on 07/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NASAAPIs/NASAAPIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAAPIsApp.swift
+//  NASAAPIs
+//
+//  Created by Ivan Quintana on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct NASAAPIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
